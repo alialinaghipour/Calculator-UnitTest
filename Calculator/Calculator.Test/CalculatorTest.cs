5a42e4aebf20e97641d488df40e15d3e71d85751
alimmst <79074714+alimmst@users.noreply.github.com>
2021-03-23 11:37:50 +0430

    added-test-for-plus

---
+++ Calculator/Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorTest()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TestMethod]
+        public void PlusCalculatorTest()
+        {
+            var plus = _calculator.Plus(8, 4);
+
+            Assert.AreEqual(12, plus);
+        }
+    }
+}
